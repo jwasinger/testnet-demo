8ffb3ee49725525c7f379859fb15465412fe0f46
Jared Wasinger <j-wasinger@hotmail.com>
2018-02-28 14:33:48 +0000

    foobar

--- txn.js
+++ txn.js
@@ -1,13 +1,16 @@
 const EthereumTx = require('ethereumjs-tx')
 const privateKey = Buffer.from('d90b721461bda3adb40e5967e26bc90627b4ec30b2afee383e6b2b3103bc926e', 'hex')
+const argv = require('yargs').argv;
+
+let data = argv.data ? argv.data : ''
 
 const txParams = {
-  nonce: '0x02',
+  nonce: '0x00',
   gasPrice: '0x174876e800', 
   gasLimit: '0x100000',
-  to: '0x0000000000000000000000000000000000000420',
+  to: '',
   value: '0xa', 
-  data: '',
+  data: data,
   chainId: 66
 }
 
