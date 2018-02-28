fed26d392c4a87ed976a6694e57e870cea8aa095
Jared Wasinger <j-wasinger@hotmail.com>
2018-02-28 14:22:03 +0000

    more stuff

--- generate_init_wasm.sh
+++ generate_init_wasm.sh
@@ -2,8 +2,13 @@
 
 # generate wasm bytecode which, when attached to a transaction, will create an account with the specified code
 
+set -e
+
 CONTRACT_WASM="$(cat $1 | xxd -p -c 100000)"
 let WASM_SIZE=${#CONTRACT_WASM}/2
+TMP_FILE=$(mktemp)
+
+trap 'rm -f -- "$TMP_FILE"' INT TERM HUP EXIT
 
 echo "(module \
 	(import \"ethereum\" \"return\" (func \$return (param i32 i32))) \
@@ -16,4 +21,6 @@ echo "(module \
 			(i32.const $WASM_SIZE) \
 		) \
 	) \
-)"
+)" > $TMP_FILE
+
+wasm-as -o txn-wasm.wasm $TMP_FILE
