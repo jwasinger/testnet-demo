88270e754e05d19f2f5c28e907fa2bdd583a0809
Jared Wasinger <j-wasinger@hotmail.com>
2018-02-28 13:43:20 +0000

    add script to generate wasm which can be used transaction data to create a contract with specified code

---
+++ generate_init_wasm.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# generate wasm bytecode which, when attached to a transaction, will create an account with the specified code
+
+CONTRACT_WASM="$(cat $1 | xxd -p -c 100000)"
+let WASM_SIZE=${#CONTRACT_WASM}/2
+
+echo $CONTRACT_WASM
+
+echo "(module \
+	(import \"ethereum\" \"return\" (func \$return (param i32 i32))) \
+	(memory 1) \
+  (data (i32.const 0)  \"$(python endian-swap/swap.py $CONTRACT_WASM)\") \
+	(export \"main\" (func \$main)) \
+	(func \$main \
+		(call \$return \
+			(i32.const 0) \
+			(i32.const $WASM_SIZE) \
+		) \
+	) \
+)"
