a4b66cb03f9fdc3670145669f9789187ff854a49
Jared Wasinger <j-wasinger@hotmail.com>
2018-03-01 17:37:50 +0000

    return correct size

--- generate_init_wasm.sh
+++ generate_init_wasm.sh
@@ -20,7 +20,7 @@ echo "(module \
     (call \$printMemHex (i32.const 0) (i32.const $WASM_SIZE)) \
 		(call \$return \
 			(i32.const 0) \
-			(i32.const 1) \
+			(i32.const $WASM_SIZE) \
 		) \
 	) \
 )" > $TMP_FILE
